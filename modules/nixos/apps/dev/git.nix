c3ccf067bd71dea5f3495e6bde7df7fd08c77c0f
btmxh <ngoduyanh.chip@gmail.com>
2026-01-06 17:01:57 +0900

    fix: keep user config + fix git

--- modules/nixos/apps/dev/git.nix
+++ modules/nixos/apps/dev/git.nix
@@ -5,19 +5,48 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib)
+    mkEnableOption
+    mkOption
+    mkIf
+    types
+    ;
   inherit (config.mine) user;
   cfg = config.mine.apps.dev.git;
 in
 {
   options.mine.apps.dev.git = {
     enable = mkEnableOption "Enable Git";
+    userName = mkOption {
+      type = types.str;
+      description = "Git username";
+      default = "defaultgit";
+    };
+    userEmail = mkOption {
+      type = types.str;
+      description = "Git email";
+      default = "defaultgit@git.dev";
+    };
+    defaultBranch = mkOption {
+      type = types.str;
+      description = "Default branch";
+      default = "master";
+    };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      git
-      gh
-    ];
+    home-manager.users.${user.name} = {
+      programs.gh.enable = true;
+      programs.git = {
+        enable = true;
+        settings = {
+          user = {
+            name = cfg.userName;
+            email = cfg.userEmail;
+          };
+          init.defaultBranch = cfg.defaultBranch;
+        };
+      };
+    };
   };
 }
