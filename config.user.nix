03a6e7eee03a0d0a452cf4279fb54cbd0d27b588
btmxh <ngoduyanh.chip@gmail.com>
2026-03-16 11:28:47 +0900

    feat(nix-ld): enable nix-ld for uv

--- config.user.nix
+++ config.user.nix
@@ -178,6 +178,8 @@
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
 
+    programs.nix-ld.enable = true;
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
