c24c9a38305e08312e6388ea3c8d7a704a4f1aa8
btmxh <ngoduyanh.chip@gmail.com>
2026-05-08 13:52:20 +0900

    chore: hyprland work monitor setup

--- modules/nixos/apps/wm/hyprland.nix
+++ modules/nixos/apps/wm/hyprland.nix
@@ -77,15 +77,21 @@ in
           exec-once = waybar
           exec-once = [workspace 10 silent] discord
 
-          monitor = eDP-1, 1920x1080@165, auto, 1
-          monitor = HDMI-A-1, 1920x1080@75, auto-left, 1
+          monitor = eDP-1, 1920x1080@165, 0x0, 1
+          monitor = HDMI-A-1, 1920x1080@75, -1920x0, 1
+          monitor = DP-6, 2560x1440@120, 0x-1440, 1
+          monitor = DP-5, 2560x1440@120, -2560x-1440, 1
+          monitor = ,preferred,auto,1
+
           # Work browser
-          workspace = 1, default:true
+          workspace = 1, monitor:DP-5, default:true
           # Work (coding, art, etc.)
           # workspace = 2, monitor:eDP-1 # project 1
           # workspace = 3, monitor:eDP-1 # extra project 1
+          # workspace = 3, monitor:DP-6, default:true
           # workspace = 4, monitor:eDP-1 # project 2
           # workspace = 5, monitor:eDP-1 # extra project 2
+          workspace = 5, monitor:DP-6
           # Temporaries
           workspace = 6, monitor:eDP-1
           # System stuff
